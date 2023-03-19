a82f194389c758336fc470e509c9b8281b373482
fhwattiheluw <fadliwattiheluw1994@gmail.com>
2023-03-20 00:32:39 +0900

    update view /user/login dan route

--- resources/views/user/login.blade.php
+++ resources/views/user/login.blade.php
@@ -1,19 +1,91 @@
 <!DOCTYPE html>
-  <html>
-    <head>
-      <!--Import Google Icon Font-->
-      <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-      <!--Import materialize.css-->
-      <link type="text/css" rel="stylesheet" href="css/materialize.min.css"  media="screen,projection"/>
+<html>
+  <head>
+    <!--Import Google Icon Font-->
+    <link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <!--Import materialize.css-->
+    <link type="text/css" rel="stylesheet" href="assets/materialize/css/materialize.min.css" media="screen,projection"/>
 
-      <!--Let browser know website is optimized for mobile-->
-      <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-    </head>
+    <!-- QUERYMINE Page Center Css -->
 
-    <body>
-      <h1>LOGIN</h1>
-      
-      <!--JavaScript at end of body for optimized loading-->
-      <script type="text/javascript" src="js/materialize.min.js"></script>
-    </body>
-  </html>
+    <style>
+      html, body {
+            height: 100%;
+        }
+      html {
+            display: table;
+            margin: auto;
+      }
+        body {
+            display: table-cell;
+            vertical-align: middle;
+      }
+    </style>
+
+    <!--Let browser know website is optimized for mobile-->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+  </head>
+
+  <body class="blue-grey darken-3">
+
+        <!-- Form Section -->
+
+      <form action="#" method="POST"> <!-- Change The Form Method From Here-->
+        <!--csrf untuk proteksi saat mengirim data -->
+        @csrf 
+
+    <div class="card-panel z-depth-5">
+
+        <!-- Form Logo Section  -->
+
+        <div class="row margin">
+            <div class="col s12 m12 l12 center">
+                <img src="assets/img/user.png" alt="" class="responsive-img circle" style="width:100px;">
+                <p>Login </p>
+            </div>
+        </div>
+
+        <!-- Form Username Input Section -->
+
+        <div class="col s12 m12 l12">
+            <div class="input-field">
+                <i class="material-icons prefix">person</i>
+                <input type="text" name="username" id="username">
+                <label for="username">Email</label>
+            </div>
+        </div>
+
+        <!-- Form Password Input Section -->
+
+        <div class="col m12 l12">
+            <div class="input-field">
+                <i class="material-icons prefix">lock</i>
+                <input type="password" name="password" id="password">
+                <label for="password">Password</label>
+            </div>
+        </div>
+
+        <br><br>
+
+            <!-- Form Button Section  -->
+
+        <div class="center">
+            <input type="submit" value="Login" name="login" 
+            class="btn waves-effect waves-light " 
+            style="width:100%; background-color: #ff4081;">
+        </div>
+
+            <!-- Form "Register Now" And "Forgot Password" Link Section. -->
+
+        <div class="" style="font-size:14px;"><br>
+            <a href="#" class="left">Register Now!</a>
+            <a href="#" class="right ">Forgot Password?</a>
+          </div><br>
+        </div>
+    </form>
+
+    <!--Import jQuery before materialize.js-->
+    <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+    <script type="text/javascript"  src="assets/materialize/js/materialize.min.js"></script>
+  </body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -3,20 +3,17 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PaketWoController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+// ROUTE AWAL 
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('user/login');
 });
+
+// Halaman User
+
+
+// Halaman admin
+
 
 Route::get('/login', 'App\Http\Controllers\UserController@showLoginForm')->name('login');
 Route::post('/login', 'App\Http\Controllers\UserController@login')->name('login.post');
