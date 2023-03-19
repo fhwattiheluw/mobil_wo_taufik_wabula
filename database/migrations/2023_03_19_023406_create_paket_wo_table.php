7f380b4b2d786f01e9b1563406984782494b0fac
lapitabatulatiga <lapita.batulatiga@gmail.com>
2023-03-19 11:44:18 +0900

    ok

---
+++ database/migrations/2023_03_19_023406_create_paket_wo_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePaketWoTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('paket_wo', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('id_user')->constrained('users');
+            $table->string('nama_paket')->nullable(false);
+            $table->unsignedDecimal('harga', 10, 2)->nullable(false);
+            $table->text('spesifikasi')->nullable(false);
+            $table->enum('status', ['aktif', 'nonaktif'])->default('nonaktif');
+            $table->string('foto_paket1')->nullable(false);
+            $table->string('foto_paket2')->nullable(true);
+            $table->string('foto_paket3')->nullable(true);
+            $table->string('foto_paket4')->nullable(true);
+            $table->timestamps();
+        });
+    }
+
+
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('paket_wo');
+    }
+}
--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -319,8 +319,8 @@ class ComposerStaticInit2b9b241250ead4894dbc755fd17b2f5a
         ),
         'Psr\\Http\\Message\\' => 
         array (
-            0 => __DIR__ . '/..' . '/psr/http-message/src',
-            1 => __DIR__ . '/..' . '/psr/http-factory/src',
+            0 => __DIR__ . '/..' . '/psr/http-factory/src',
+            1 => __DIR__ . '/..' . '/psr/http-message/src',
         ),
         'Psr\\Http\\Client\\' => 
         array (
@@ -499,6 +499,7 @@ class ComposerStaticInit2b9b241250ead4894dbc755fd17b2f5a
         'App\\Console\\Kernel' => __DIR__ . '/../..' . '/app/Console/Kernel.php',
         'App\\Exceptions\\Handler' => __DIR__ . '/../..' . '/app/Exceptions/Handler.php',
         'App\\Http\\Controllers\\Controller' => __DIR__ . '/../..' . '/app/Http/Controllers/Controller.php',
+        'App\\Http\\Controllers\\UserController' => __DIR__ . '/../..' . '/app/Http/Controllers/UserController.php',
         'App\\Http\\Kernel' => __DIR__ . '/../..' . '/app/Http/Kernel.php',
         'App\\Http\\Middleware\\Authenticate' => __DIR__ . '/../..' . '/app/Http/Middleware/Authenticate.php',
         'App\\Http\\Middleware\\EncryptCookies' => __DIR__ . '/../..' . '/app/Http/Middleware/EncryptCookies.php',
