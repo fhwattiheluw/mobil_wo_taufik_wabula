518f20c1205367c6840a14d6259b87015ecc99be
lapitabatulatiga <lapita.batulatiga@gmail.com>
2023-03-27 15:09:06 +0900

    fungsi tombol booking ta kirim ka WA

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -21,7 +21,7 @@ class DatabaseSeeder extends Seeder
         DB::table('users')->insert([
             'nama' => 'Pengguna 1',
             'email' => 'pengguna1@mail.com',
-            'nomor_hp' => '08123456789',
+            'nomor_hp' => '+628123456789',
             'alamat' => 'Jl. Sudirman No. 1',
             'foto' => 'pengguna1.jpg',
             'jenis_user' => 'pengguna',
@@ -35,7 +35,7 @@ class DatabaseSeeder extends Seeder
         DB::table('users')->insert([
             'nama' => 'Pengguna 2',
             'email' => 'pengguna2@mail.com',
-            'nomor_hp' => '08123456790',
+            'nomor_hp' => '+628123456790',
             'alamat' => 'Jl. Sudirman No. 2',
             'foto' => 'pengguna2.jpg',
             'jenis_user' => 'pengguna',
@@ -49,7 +49,7 @@ class DatabaseSeeder extends Seeder
         DB::table('users')->insert([
             'nama' => 'Pengguna 3',
             'email' => 'pengguna3@mail.com',
-            'nomor_hp' => '08123456791',
+            'nomor_hp' => '+628123456791',
             'alamat' => 'Jl. Sudirman No. 3',
             'foto' => 'pengguna3.jpg',
             'jenis_user' => 'pengguna',
@@ -64,7 +64,7 @@ class DatabaseSeeder extends Seeder
         DB::table('users')->insert([
             'nama' => 'WO 1',
             'email' => 'wo1@mail.com',
-            'nomor_hp' => '08123456792',
+            'nomor_hp' => '+628123456792',
             'alamat' => 'Jl. Thamrin No. 1',
             'foto' => 'wo1.jpg',
             'jenis_user' => 'wo',
@@ -78,7 +78,7 @@ class DatabaseSeeder extends Seeder
         DB::table('users')->insert([
             'nama' => 'WO 2',
             'email' => 'wo2@mail.com',
-            'nomor_hp' => '08123456793',
+            'nomor_hp' => '+628123456793',
             'alamat' => 'Jl. Thamrin No. 2',
             'foto' => 'wo2.jpg',
             'jenis_user' => 'wo',
@@ -91,7 +91,7 @@ class DatabaseSeeder extends Seeder
         DB::table('users')->insert([
             'nama' => 'WO 3',
             'email' => 'wo3@mail.com',
-            'nomor_hp' => '08123456794',
+            'nomor_hp' => '+628123456794',
             'alamat' => 'Jl. Thamrin No. 3',
             'foto' => 'wo3.jpg',
             'jenis_user' => 'wo',
