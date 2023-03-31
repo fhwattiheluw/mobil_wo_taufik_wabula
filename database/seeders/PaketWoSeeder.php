f7c8e9ac774c7c4bffede91767b810af7643226f
lapitabatulatiga <lapita.batulatiga@gmail.com>
2023-03-31 12:58:05 +0900

    seed paket wo

--- database/seeders/PaketWoSeeder.php
+++ database/seeders/PaketWoSeeder.php
@@ -21,8 +21,7 @@ class PaketWoSeeder extends Seeder
         $paket1->harga = 1000000.00;
         $paket1->spesifikasi = 'Paket A spesifikasi';
         $paket1->status = 'aktif';
-        $paket1->foto_paket1 = 'paket-a-foto1.jpg';
-        $paket1->foto_paket2 = 'paket-a-foto2.jpg';
+        $paket1->foto_paket = 'paket-a-foto1.jpg';
         $paket1->save();
 
         $paket2 = new PaketWo();
@@ -32,7 +31,7 @@ class PaketWoSeeder extends Seeder
         $paket2->harga = 1500000.00;
         $paket2->spesifikasi = 'Paket B spesifikasi';
         $paket2->status = 'aktif';
-        $paket2->foto_paket1 = 'paket-b-foto1.jpg';
+        $paket2->foto_paket = 'paket-b-foto1.jpg';
         $paket2->save();
 
         // Ambil user kedua sebagai penjual paket
@@ -46,10 +45,7 @@ class PaketWoSeeder extends Seeder
         $paket3->harga = 2000000.00;
         $paket3->spesifikasi = 'Paket C spesifikasi';
         $paket3->status = 'aktif';
-        $paket3->foto_paket1 = 'paket-c-foto1.jpg';
-        $paket3->foto_paket2 = 'paket-c-foto2.jpg';
-        $paket3->foto_paket3 = 'paket-c-foto3.jpg';
-        $paket3->foto_paket4 = 'paket-c-foto4.jpg';
+        $paket3->foto_paket = 'paket-c-foto1.jpg';
         $paket3->save();
 
         $paket4 = new PaketWo();
@@ -59,7 +55,7 @@ class PaketWoSeeder extends Seeder
         $paket4->harga = 2500000.00;
         $paket4->spesifikasi = 'Paket D spesifikasi';
         $paket4->status = 'aktif';
-        $paket4->foto_paket1 = 'paket-d-foto1.jpg';
+        $paket4->foto_paket = 'paket-d-foto1.jpg';
         $paket4->save();
     }
 }
