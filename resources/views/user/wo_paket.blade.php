6935865e7bcbd97e03cdfd07f82872304b99173c
fhwattiheluw <fadliwattiheluw1994@gmail.com>
2023-05-02 12:08:55 +0900

    update paket

--- resources/views/user/wo_paket.blade.php
+++ resources/views/user/wo_paket.blade.php
@@ -32,20 +32,24 @@
 
   <div class="row">
     @foreach($pakets as $paket)
-        <div class="card">
-          <div class="card-image waves-effect waves-block waves-light">
-            <img class="activator" src="/assets/img/paket1.jpg">
-          </div>
-          <div class="card-content">
-            <span class="card-title activator grey-text text-darken-4">{{ $paket->nama_paket; }}<i class="material-icons right">zoom_out_map</i></span>
-            <p><a href="#">{{ $paket->user->nama; }}</a></p>
-          </div>
-          <div class="card-reveal">
-            <span class="card-title grey-text text-darken-4">{{ $paket->nama_paket }}<i class="material-icons right">close</i></span>
-            <p>{{ $paket->spesifikasi }}</p>
-            <p><a href="https://api.whatsapp.com/send?phone={{ $paket->user->nomor_hp }}&text=Apakah%bisa%pesan%paket%ini%?">Booking</a></p>
-          </div>
-        </div>
+    <div class="card">
+      <div class="card-image waves-effect waves-block waves-light">
+        <!-- <img class="activator" src="/assets/img/paket1.jpg"> -->
+        <img class="activator" src="{{url(Storage::url('public/img/'.$paket->foto_paket))}}">
+
+      </div>
+      <div class="card-content">
+        <span class="card-title activator grey-text text-darken-4">{{ $paket->nama_paket }}<span class="new badge" data-badge-caption="paket">{{$paket->jenis}}</span></span>
+        <p><a href="{{route('user.wo.about',['id_wo'=>$paket->id_user])}}">{{ $paket->user->nama }}</a></p> <!-- redirect ke halaman tentang WO -->
+      </div>
+      <div class="card-reveal">
+        <span class="card-title grey-text text-darken-4">{{ $paket->nama_paket }}<i class="material-icons right">close</i></span>
+        <p>{{ $paket->jenis_paket }}</p>
+        <p>Rp.{{ $paket->harga }}</p>
+        <p>{{ $paket->spesifikasi }}</p>
+        <p><a class="waves-effect pink lighten-3 btn btn-small" href="https://api.whatsapp.com/send?phone={{ $paket->user->nomor_hp }}&text=Apakah bisa pesan paket ini ?"><i class="material-icons right">book</i>Booking</a></p>
+      </div>
+    </div>
     @endforeach
   </div>
 
